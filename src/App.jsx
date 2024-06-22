ce8ce60ba7b96b4e46c834cba9900c9605c56c58
Jose Everton Mota Rodrigues <jose6135@DIGITALCOLLEGE.LOCAL>
2024-06-22 11:50:24 -0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,68 @@
-import { useState } from 'react'
+function Header({children}) {
 
+  return (
+    <header className="bg-blue-800">
+      <nav className="mx-auto flex items-center p-6 shadow-md">
+        {children}
+        
+
+      </nav>
+
+    </header>
+  )
+
+}
+
+function HeaderLink({href, children}) {
+  return (
+    <a href={href} className="text-xl font-semibold leading-6 text-white ml-5">{children}</a>
+  )
+}
 
 function App() {
   
 
   return (
     
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <>
+    <Header>
+    <HeaderLink href="https://company">Company</HeaderLink>
+    <HeaderLink href="https://marketplace">Marketplace</HeaderLink>
+    <HeaderLink href="https://teste">Teste</HeaderLink>
+    </Header>
+    </>
+    
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App
